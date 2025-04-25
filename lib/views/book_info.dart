03751315c8c16c65736c41d6a648c5b4604d4f8f
Ahmedeldesoky703 <eldesokyahmed51@gmail.com>
2025-04-25 21:02:40 +0300

    book enfo screen

---
+++ lib/views/book_info.dart
@@ -0,0 +1,200 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class BookInfo extends StatefulWidget {
+  const BookInfo({super.key});
+
+  @override
+  State<BookInfo> createState() => _BookInfoState();
+}
+
+class _BookInfoState extends State<BookInfo> {
+  final TextEditingController _statusController = TextEditingController();
+  DateTime? _startDate;
+  DateTime? _endDate;
+
+  Future<void> _selectDate(BuildContext context, bool isStartDate) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+    if (picked != null && picked != (isStartDate ? _startDate : _endDate)) {
+      setState(() {
+        if (isStartDate) {
+          _startDate = picked;
+        } else {
+          _endDate = picked;
+        }
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 40),
+              Container(
+                // the image of the book will be here
+                height: 150,
+                width: 150,
+                color: Colors.grey[300],
+                child: Stack(),
+              ),
+              SizedBox(height: 16),
+              // Book name
+              Text(
+                'Name book',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              // Author
+              Text(
+                'Author: ',
+                style: TextStyle(fontSize: 18, color: Colors.grey[700]),
+              ),
+              SizedBox(height: 16),
+              // Summary label
+              Text(
+                'Summary book',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              // Summary text field
+              TextFormField(
+                maxLines: 5,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: BorderSide(color: Colors.grey[400]!),
+                  ),
+                  hintText: 'Enter summary here...',
+                ),
+              ),
+              SizedBox(height: 16),
+              // Rating
+              Row(
+                children: [
+                  Text(
+                    'Rating: ',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    children: List.generate(
+                      5,
+                      (index) =>
+                          Icon(Icons.star, color: Colors.yellow[700], size: 24),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              TextField(
+                controller: _statusController,
+                decoration: InputDecoration(
+                  labelText: 'Reading Status',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 16.0),
+
+              Text('Start date', style: TextStyle(fontWeight: FontWeight.bold)),
+              SizedBox(height: 8.0),
+              GestureDetector(
+                onTap: () => _selectDate(context, true),
+                child: Container(
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        _startDate == null
+                            ? 'mm/dd/yyyy'
+                            : DateFormat('MM/dd/yyyy').format(_startDate!),
+                        style: TextStyle(
+                          color:
+                              _startDate == null ? Colors.grey : Colors.black,
+                        ),
+                      ),
+                      Icon(Icons.calendar_today, color: Colors.grey),
+                    ],
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _startDate = null;
+                      });
+                    },
+                    child: Text('Clear'),
+                  ),
+                  TextButton(onPressed: () {}, child: Text('Cancel')),
+                  TextButton(onPressed: () {}, child: Text('OK')),
+                ],
+              ),
+              SizedBox(height: 16.0),
+
+              Text('End date', style: TextStyle(fontWeight: FontWeight.bold)),
+              SizedBox(height: 8.0),
+              GestureDetector(
+                onTap: () => _selectDate(context, false),
+                child: Container(
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        _endDate == null
+                            ? 'mm/dd/yyyy'
+                            : DateFormat('MM/dd/yyyy').format(_endDate!),
+                        style: TextStyle(
+                          color: _endDate == null ? Colors.grey : Colors.black,
+                        ),
+                      ),
+                      Icon(Icons.calendar_today, color: Colors.grey),
+                    ],
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _endDate = null;
+                      });
+                    },
+                    child: Text('Clear'),
+                  ),
+                  TextButton(onPressed: () {}, child: Text('Cancel')),
+                  TextButton(onPressed: () {}, child: Text('OK')),
+                ],
+              ),
+              SizedBox(height: 16.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
