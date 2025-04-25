e98a861ed539b58ae17a5c4adc53569dce62b9d1
mohamed ahmad <amohamedahmad68@gmail.com>
2025-04-25 22:38:39 +0300

    add custome search bar search file

--- lib/views/search_screen.dart
+++ lib/views/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:book_trail/views/widgets/custom_app_bar_search.dart';
+import 'package:book_trail/views/widgets/custom_search_bar_search.dart';
 import 'package:flutter/material.dart';
 
 class SearchScreen extends StatelessWidget {
@@ -8,26 +9,7 @@ class SearchScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBarSearch(),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: TextField(
-          onTap: () => debugPrint("Search now"),
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: const Color(0xFFECE6F0),
-            hintText: "search text",
-            prefixIcon: Icon(Icons.search),
-            suffixIcon: Padding(
-              padding: const EdgeInsets.only(right: 15),
-              child: Icon(Icons.qr_code_scanner),
-            ),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(50),
-              borderSide: BorderSide.none,
-            ),
-          ),
-        ),
-      ),
+      body: CustomSearchBarSearch(),
     );
   }
 }
