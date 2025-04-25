92e907d04964d383d9cee1357488d05c64f9219a
mohamed ahmad <amohamedahmad68@gmail.com>
2025-04-25 20:38:01 +0300

    add search appBar and text field of search with change color in search_screen

--- lib/views/search_screen.dart
+++ lib/views/search_screen.dart
@@ -5,6 +5,32 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(252, 233, 254, 1),
+        centerTitle: true,
+        title: Text("search"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: TextField(
+          onTap: () => debugPrint("Search now"),
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: const Color(0xFFECE6F0),
+            hintText: "search text",
+            prefixIcon: Icon(Icons.search),
+            suffixIcon: Padding(
+              padding: const EdgeInsets.only(right: 15),
+              child: Icon(Icons.qr_code_scanner),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(50),
+              borderSide: BorderSide.none,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
