365fb7abecf044c6a0541caa97ab97d19d8ee094
mohamed ahmad <amohamedahmad68@gmail.com>
2025-04-25 23:25:53 +0300

    add custom book card and book list view and Edit File Search

--- lib/views/search_screen.dart
+++ lib/views/search_screen.dart
@@ -1,20 +1,37 @@
+import 'package:book_trail/views/widgets/book_list_view.dart';
 import 'package:book_trail/views/widgets/custom_app_bar_search.dart';
 import 'package:book_trail/views/widgets/custom_navigation_bar.dart';
 import 'package:book_trail/views/widgets/custom_search_bar_search.dart';
 import 'package:flutter/material.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final List<Book> books = [
+    Book(title: "name book", author: "author", status: "read"),
+    Book(title: "name book", author: "author", status: "want to read"),
+    Book(title: "name book", author: "author", status: "reading"),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBarSearch(),
-      body: CustomSearchBarSearch(),
+      body: Column(
+        children: [
+          CustomSearchBarSearch(),
+          Expanded(child: BookListView(books: books)),
+        ],
+      ),
       bottomNavigationBar: CustomBottomNavigationBar(
-          currentIndex: 3,
-          onTap: (index) {},
-        ),
+        currentIndex: 3,
+        onTap: (index) {},
+      ),
     );
   }
 }
