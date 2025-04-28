1abe25c7f24cd4bfb88e83950d6267ba3e098921
mohamed ahmad <amohamedahmad68@gmail.com>
2025-04-28 03:56:03 +0300

    fix sonar

---
+++ lib/views/widgets/stats_search/book_list_view.dart
@@ -0,0 +1,41 @@
+
+import 'package:book_trail/views/widgets/stats_search/custom_book_card.dart';
+import 'package:flutter/material.dart';
+
+class BookListView extends StatefulWidget {
+  final List<Book> books;
+
+  const BookListView({super.key, required this.books});
+
+  @override
+  State<BookListView> createState() => _BookListViewState();
+}
+
+class _BookListViewState extends State<BookListView> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      padding: EdgeInsets.zero,
+      itemCount: widget.books.length,
+      itemBuilder: (context, index) {
+        final book = widget.books[index];
+        return BookCard(
+          title: book.title,
+          author: book.author,
+          status: book.status,
+          icon: Icons.star_border,
+        );
+      },
+    );
+  }
+}
+
+class Book {
+  final String title;
+  final String author;
+  final String status;
+
+  Book({required this.title, required this.author, required this.status});
+}
